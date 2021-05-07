d7544a1b6c7f683572de500adc71c43e72f0b962
sigridjonsson <sigge.jonsson@hotmail.se>
2021-05-07 16:17:34 +0200

    Added highscore route

---
+++ app/src/Controller/HighscoreController.php
@@ -0,0 +1,22 @@
+<?php
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+/**
+ *
+ */
+class HighscoreController extends AbstractController
+{
+    /**
+     * @Route("/highscore")
+    */
+    public function highscore()
+    {
+        return $this->render('layout.html.twig', [
+            'info' => "Highscore!",
+        ]);
+    }
+}
